fa9645ade2088e1f99898955db76ddb9bd3f1b35
amandhurwey <aman.dhurwey@leapfinance.com>
2023-10-09 14:59:24 +0530

    feat: setup express

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,34 @@
-import http from "http";
-import handleRouter from "./router";
+import express from "express";
+import fs from "fs";
+import path from "path";
+//1. get the app
+const app = express();
 
-const handleServer: http.RequestListener<typeof http.IncomingMessage, typeof http.ServerResponse> = (req, res) => {
-  return handleRouter(req, res);
-  //after ending the response, it we try to write something to it, it will throw an error.
-};
+app.use((req, res, next) => {
+  console.log(`I will be called every time`);
+  next();
+});
 
-//1. create server
-const server = http.createServer(handleServer);
+app.use(`/users`, (req, res) => {
+  console.log(`Inside users path`);
+  fs.readFile(path.join(__dirname, "./data/users.txt"), "utf8", (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.end("Internal Server Error");
+      return;
+    }
+    const users = data.trim().split("\n");
+    const usersHtml = users.map((user) => `<li>${user}</li>`).join(``);
+    res.setHeader("Content-type", "text/html");
+    res.statusCode = 200;
+    return res.send(`<h1>Users List</h1> <ul>${usersHtml}</ul>`);
+  });
+});
 
-//2. Listen to server
-server.listen(3001);
+app.use(`/`, (req, res) => {
+  console.log(`Inside Root path`);
+  fs.createReadStream(path.join(__dirname, "./templates/basicForm.html")).pipe(res);
+});
+
+//listen
+app.listen(3001);
